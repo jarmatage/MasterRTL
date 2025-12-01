78681bd311de627795b384750c76544ff974f86b
Joaquin Armatage <j.armatage12@gmail.com>
2025-12-01 01:42:32 -0700

    update the ml_model module

--- src/masterrtl/ml_model/preprocess.py
+++ src/masterrtl/ml_model/preprocess.py
@@ -1,44 +1,35 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from sklearn import preprocessing, svm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
-import json, pickle
-from scipy.stats import stats
+import json
 
+import numpy as np
 
 bench_type = "timing"
 feat_type = "feat_timing"
 
-def load_data_test(label_tpe):
+
+def load_data(label_tpe):
     if label_tpe == "Area":
         return load_data_area(label_tpe)
     elif label_tpe in ["TNS", "WNS"]:
         return load_data_timing(label_tpe)
     elif label_tpe == "Power":
         return load_data_power(label_tpe)
-    
-
+    raise ValueError(f"Unknown label type: {label_tpe}")
 
 
 def load_data_area(label_tpe):
-    design_name = 'TinyRocket'
-    feat_dir = f'../../example/feature/'
+    design_name = "TinyRocket"
+    feat_dir = "../../example/feature/"
 
-    with open(f'{feat_dir}{design_name}_sog_vec_area.json', 'r') as f:
+    with open(f"{feat_dir}{design_name}_sog_vec_area.json") as f:
         feat_design_lst = json.load(f)
     test_feat_lst = []
     test_feat_lst.extend(feat_design_lst)
 
-    label_dir = f'../../example/label/'
-    with open(f'{label_dir}{design_name}.json', 'r') as f:
+    label_dir = "../../example/label/"
+    with open(f"{label_dir}{design_name}.json") as f:
         label_dct = json.load(f)
     label = label_dct[label_tpe]
     test_label_lst = [label]
-
 
     test_x = np.array(test_feat_lst).reshape(1, -1)
     test_y = np.array(test_label_lst)
@@ -47,23 +38,22 @@ def load_data_area(label_tpe):
 
 
 def load_data_timing(label_tpe):
-    design_name = 'TinyRocket'
-    feat_dir = f'../../example/feature/'
+    design_name = "TinyRocket"
+    feat_dir = "../../example/feature/"
 
-    with open(f'{feat_dir}{design_name}_sog_vec_timing.json', 'r') as f:
+    with open(f"{feat_dir}{design_name}_sog_vec_timing.json") as f:
         feat_timing_lst = json.load(f)
-    with open(f'{feat_dir}{design_name}_sog_vec_area.json', 'r') as f:
+    with open(f"{feat_dir}{design_name}_sog_vec_area.json") as f:
         feat_design_lst = json.load(f)
     test_feat_lst = []
     test_feat_lst.extend(feat_design_lst)
     test_feat_lst.extend(feat_timing_lst)
 
-    label_dir = f'../../example/label/'
-    with open(f'{label_dir}{design_name}.json', 'r') as f:
+    label_dir = "../../example/label/"
+    with open(f"{label_dir}{design_name}.json") as f:
         label_dct = json.load(f)
     label = label_dct[label_tpe]
     test_label_lst = [label]
-
 
     test_x = np.array(test_feat_lst).reshape(1, -1)
     test_y = np.array(test_label_lst)
@@ -72,20 +62,19 @@ def load_data_timing(label_tpe):
 
 
 def load_data_power(label_tpe):
-    design_name = 'TinyRocket'
-    feat_dir = f'../../example/feature/'
+    design_name = "TinyRocket"
+    feat_dir = "../../example/feature/"
 
-    with open(f'{feat_dir}{design_name}_sog_vec_pwr.json', 'r') as f:
+    with open(f"{feat_dir}{design_name}_sog_vec_pwr.json") as f:
         feat_design_lst = json.load(f)
     test_feat_lst = []
     test_feat_lst.extend(feat_design_lst)
 
-    label_dir = f'../../example/label/'
-    with open(f'{label_dir}{design_name}.json', 'r') as f:
+    label_dir = "../../example/label/"
+    with open(f"{label_dir}{design_name}.json") as f:
         label_dct = json.load(f)
     label = label_dct[label_tpe]
     test_label_lst = [label]
-
 
     test_x = np.array(test_feat_lst).reshape(1, -1)
     test_y = np.array(test_label_lst)
