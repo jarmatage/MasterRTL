b4363f2c3ea28ef9d39e8ca1866867ad44b39ea0
Joaquin Armatage <j.armatage12@gmail.com>
2025-12-02 02:40:07 -0700

    add preproc commands

--- example/run.sh
+++ example/run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-echo "Process RTL"
 masterrtl yosys generate-sog \
     --design-name Rocket \
     --output-dir results \
     verilog/TinyRocket/plusarg_reader.v \
     verilog/TinyRocket/chipyard.TestHarness.TinyRocketConfig.top.v
 
-echo "Verilog to Graph"
 masterrtl yosys analyze-verilog \
     --design-name Rocket \
     --output-dir results/ \
     results/Rocket_sog.v
+
+masterrtl preproc delay \
+    --design-name Rocket \
+    --input-dir results/ \
+    --output-dir results/timing_dag/
+
+masterrtl preproc toggle-rate \
+    --bench chipyard \
+    --design-hier-json design_hier.json \
+    --module-dir module/TinyRocket/ \
+    --init-tr-dir module/TinyRocket_init_tr/ \
+    --output-dir results/power_dag/
